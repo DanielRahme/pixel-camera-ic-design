e8a904e7bfd3ef18a63b2c5ba763efb8a9fad366
Daniel Rahme <daniel.rahme.dr@gmail.com>
2019-10-28 15:39:18 +0100

    Added digital control circuit with top-level module

---
+++ Digital/Pixel_Electronics.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.10.2019 15:06:34
+// Design Name: 
+// Module Name: Pixel_Electronics
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Pixel_Electronics(
+    input NRE_1,
+    input NRE_2,
+    input ADC,
+    input Expose,
+    input Erase,
+    output ADC_1_out,
+    output ADC_2_out
+    );
+endmodule
---
+++ Digital/RE_Control.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.10.2019 15:06:34
+// Design Name: 
+// Module Name: RE_Control
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RE_Control(
+    input Exp_increase,
+    input Exp_decrease,
+    input Init,
+    input Reset,
+    input Clk,
+    output NRE_1,
+    output NRE_2,
+    output ADC,
+    output Expose,
+    output Erase
+    );
+endmodule
